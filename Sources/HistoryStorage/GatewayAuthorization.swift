ab07531f66e35e1a2351f345401c9e7cd32ed762
GuangDai <59678726+GuangDai@users.noreply.github.com>
2026-09-07 06:45:51 +0800

    Return the selected external mutation descriptor

--- Sources/HistoryStorage/GatewayAuthorization.swift
+++ Sources/HistoryStorage/GatewayAuthorization.swift
@@ -85,7 +85,7 @@ extension ExternalOperationDescriptor {
             ? .organize : .manage
         let delete: ExternalCapability = expectedConnectionKind == .localAutomation
             ? .deleteItem : .manage
-        switch request {
+        return switch request {
         case .pin(let id):
             Self(
                 capability: organize,
